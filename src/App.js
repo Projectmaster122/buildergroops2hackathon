b6bb6b894e5ffda33d7f1e07b6bc7cce583b25e5
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-09-02 15:09:01 -0400

    Added ace editor.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import AceEditor from 'react-ace'
+
+import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/theme-dracula'
+import 'ace-builds/src-noconflict/ext-language_tools'
+import 'ace-builds/src-noconflict/ext-beautify'
 import './App.css';
 
+const element = <div className='navbar'></div>;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    return (
+      element,
+        <AceEditor
+            style={{
+                height: '95vh',
+                width: '95vw',
+                left: '5vw',
+                top: '5vh'
+            }}
+            placeholder = 'Start Coding...'
+            mode='javascript'
+            theme='dracula'
+            name='basic-code-editor'
+            fontSize = { 24 }
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine = {true}
+            setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: true,
+                showLineNumbers: true,
+                tabSize: 4,
+            }}
+        />
+    )
 }
 
 export default App;
